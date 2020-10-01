9006e87fc46084a6391013e7639de777bf6444d7
BazMaster <bazilio100lei@gmail.com>
2020-10-01 07:47:59 +0300

    Homework 5

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-const $btn_kick = document.getElementById('btn-kick');
-const $btn_flash = document.getElementById('btn-flash');
+function $getElById(id) {
+    return document.getElementById(id);
+}
+
+const $btn_kick = $getElById('btn-kick');
+const $btn_flash = $getElById('btn-flash');
 
 const character = {
     name: 'Pikachu',
     defaultHP: 200,
     damageHP: 200,
     progress: 100,
-    elHP: document.getElementById('health-character'),
-    elProgressbar: document.getElementById('progressbar-character'),
-    changeHP: changeHP,
-    renderHP: renderHP,
-    renderHPLife: renderHPLife,
-    renderProgressbarHP: renderProgressbarHP,
+    elHP: $getElById('health-character'),
+    elProgressbar: $getElById('progressbar-character'),
+    elLog: '#log-1',
+    changeHP,
+    renderHP,
+    renderHPLife,
+    renderProgressbarHP,
 }
 
 const enemy = {
@@ -19,12 +24,13 @@ const enemy = {
     defaultHP: 200,
     damageHP: 200,
     progress: 100,
-    elHP: document.getElementById('health-enemy'),
-    elProgressbar: document.getElementById('progressbar-enemy'),
-    changeHP: changeHP,
-    renderHP: renderHP,
-    renderHPLife: renderHPLife,
-    renderProgressbarHP: renderProgressbarHP,
+    elHP: $getElById('health-enemy'),
+    elProgressbar: $getElById('progressbar-enemy'),
+    elLog: '#log-2',
+    changeHP,
+    renderHP,
+    renderHPLife,
+    renderProgressbarHP,
 }
 
 init();
@@ -47,22 +53,25 @@ function clickBtn(num) {
 }
 
 function changeHP(count) {
+    const { name, defaultHP } = this;
     let progress;
-    progress = count / this.defaultHP * 100;
+    progress = count / defaultHP * 100;
     progress = this.progress - Math.ceil(progress);
 
-    if(this.damageHP < count) {
+    this.damageHP -= count;
+
+    if(this.damageHP <= 0) {
         this.damageHP = 0;
         progress = 0;
-        alert('Бедный ' + this.name + ' проиграл бой!');
+        alert('Бедный ' + name + ' проиграл бой!');
         $btn_kick.disabled = true;
         $btn_flash.disabled = true;
     }
-    else {
-        this.damageHP -= count;
-    }
+
     this.progress = progress;
     this.renderHP();
+
+    const log = this === enemy ? generateLog(this, character, count) : generateLog(this, enemy, count);
 }
 
 function renderHP() {
@@ -82,4 +91,29 @@ function random(num) {
     return Math.ceil(Math.random() * num);
 }
 
+function generateLog(firstPerson, secondPerson, count) {
+    const { name, damageHP, progress, elLog } = firstPerson;
+    const logs = [
+        `${name} вспомнил что-то важное, но неожиданно ${secondPerson.name}, не помня себя от испуга, ударил в предплечье врага.\n\n-${count}, [${progress}/100]`,
+        `${name} поперхнулся, и за это ${secondPerson.name} с испугу приложил прямой удар коленом в лоб врага.\n\n-${count}, [${progress}/100]`,
+        `${name} забылся, но в это время наглый ${secondPerson.name}, приняв волевое решение, неслышно подойдя сзади, ударил.\n\n-${count}, [${progress}/100]`,
+        `${name} пришел в себя, но неожиданно ${secondPerson.name} случайно нанес мощнейший удар.\n\n-${count}, [${progress}/100]`,
+        `${name} поперхнулся, но в это время ${secondPerson.name} нехотя раздробил кулаком \<вырезанно цензурой\> противника.\n\n-${count}, [${progress}/100]`,
+        `${name} удивился, а ${secondPerson.name} пошатнувшись влепил подлый удар.\n\n-${count}, [${progress}/100]`,
+        `${name} высморкался, но неожиданно ${secondPerson.name} провел дробящий удар.\n\n-${count}, [${progress}/100]`,
+        `${name} пошатнулся, и внезапно наглый ${secondPerson.name} беспричинно ударил в ногу противника\n\n-${count}, [${progress}/100]`,
+        `${name} расстроился, как вдруг, неожиданно ${secondPerson.name} случайно влепил стопой в живот соперника.\n\n-${count}, [${progress}/100]`,
+        `${name} пытался что-то сказать, но вдруг, неожиданно ${secondPerson.name} со скуки, разбил бровь сопернику.\n\n-${count}, [${progress}/100]`
+    ];
+    const log = logs[random(logs.length) - 1];
+    const $p = document.createElement('p');
+    const $logs = document.querySelector(elLog);
+    $p.innerText = log;
+    $logs.insertBefore($p, $logs.children[0]);
 
+    if(damageHP === 0) {
+        $p.innerText = 'Бедный ' + name + ' проиграл бой!';
+        $p.setAttribute('class', 'fail');
+        $logs.insertBefore($p, $logs.children[0]);
+    }
+}
